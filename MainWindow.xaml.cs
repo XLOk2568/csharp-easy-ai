4300f15d5e24edc4a4d83105a8d962c34c91dc82
XiaLiang <140861510+XLOk2568@users.noreply.github.com>
2025-07-28 20:14:06 +0800

    Add files via upload

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -31,97 +31,57 @@ namespace NavigationViewExample
         {
             InitializeComponent();
         }
-        //这里说下方案，在主页弄两个frame，一个MainNv是用来作为用户主要的交互导航，另一个MainMini则是用来作为用户的进度条等等播放控制
         public Pages.MainNvPage PageNv_Load= new Pages.MainNvPage();
         private void Window_Loaded(object sender, RoutedEventArgs e)
         {
-            App.settings_ = File.ReadAllText(App.setting_path);
-            App.setting_all_list = App.settings_.Split("${@}").ToList();
-            List<string> setting_homelist = App.settings_.Split("${@}").ToList();
-            setting_homelist_count = setting_homelist.Count;
-            //读取和设置屏幕 
-            List<string> tall2 = setting_homelist[0].Split(",").ToList();                                                            
-            Double happ = Double.Parse(tall2[0]);
-            Height = happ;
-            Double wapp = Double.Parse(tall2[1]);
-            Width = wapp;
-            Double lapp = Double.Parse(tall2[2]);
-            Left = lapp;
-            Double tapp = Double.Parse(tall2[3]);
-            Top = tapp;
-            //设置主题          
-            List<string> theme = App.settings_.Split("${@}").ToList();                                                                        
-            if (setting_homelist[1] == "dark")
+            //先检测是否存在屏幕，位置的文件夹
+            string folderName = "Appdata";
+            string folderPath = System.IO.Path.Combine(AppDomain.CurrentDomain.BaseDirectory, folderName);
+            string filePath = System.IO.Path.Combine(folderPath, "data.txt");
+            string writeText = "//屏幕_121_222_333_444_";
+            if (!System.IO.Directory.Exists(folderPath))
             {
-                ThemeManager.Current.ApplicationTheme = ApplicationTheme.Dark;
+                System.IO.Directory.CreateDirectory(folderPath);                // 获取当前程序运行目录
+                File.WriteAllText(filePath, writeText);
+                MessageBox.Show("Please restart the software.");
             }
-            else if (setting_homelist[1] == "light")
+            else if (!File.Exists(filePath))
             {
-                   ThemeManager.Current.ApplicationTheme = ApplicationTheme.Light;
+                File.WriteAllText(filePath, writeText);
+                MessageBox.Show("Please restart the software.");
             }
-            //设置语言
-            if (setting_homelist[2] == "CN") //中
+            else
             {
-                App.InterfaceLanguage_All = File.ReadAllText(App.Run_Time_Path + "\\Language_S\\CN.txt");
-                App.IL_all_list = App.InterfaceLanguage_All.Split("${@}").ToList();
-            }
-            else if (setting_homelist[2] == "EN")//英文
-            {
-                App.InterfaceLanguage_All = File.ReadAllText(App.Run_Time_Path + "\\Language_S\\EN.txt");
-                App.IL_all_list = App.InterfaceLanguage_All.Split("${@}").ToList();
-            }
-            if (setting_homelist[2] == "JP")//日语
-            {
-                App.InterfaceLanguage_All = File.ReadAllText(App.Run_Time_Path + "\\Language_S\\JP.txt");
-                App.IL_all_list = App.InterfaceLanguage_All.Split("${@}").ToList();
-            }
-            if (setting_homelist[2] == "FR")//法语
-            {
-                App.InterfaceLanguage_All = File.ReadAllText(App.Run_Time_Path + "\\Language_S\\FR.txt");
-                App.IL_all_list = App.InterfaceLanguage_All.Split("${@}").ToList();
+                string listText = File.ReadAllText(filePath);
+                List<string> setting_homelist = listText.Split("_").ToList();
+                Double happ = Double.Parse(setting_homelist[1]);
+                Height = happ;
+                Double wapp = Double.Parse(setting_homelist[2]);
+                Width = wapp;
+                Double lapp = Double.Parse(setting_homelist[3]);
+                Left = lapp;
+                Double tapp = Double.Parse(setting_homelist[4]);
+                Top = tapp;
             }
             Frame_Main_MainNv.Navigate(PageNv_Load);
-
-            //异步读取自定义的列表文件，前面的以后再改了
-            Load_IL();
-        }
-        private List<string> MLO_ = new List<string>();//临时的
-        private async void Load_IL()
-        {
-            await Task.Run(() =>
-            {
-                string filePath = App.Cover_of_the_List_Path; //请确保文件路径正确
-                if (File.Exists(filePath))
-                {
-                    string content = File.ReadAllText(filePath);
-                    string[] parts = content.Split(new string[] { "${@}" }, StringSplitOptions.None);
-                    foreach (string part in parts)
-                    {
-                        MLO_.Add(part);
-                    }
-                }
-                else
-                {
-                    MessageBox.Show("文件不存在\n\nFile not found\n\nファイルが見つかりません\n\nFichier non trouvé", "!", MessageBoxButton.OK, MessageBoxImage.Warning);
-                }
-            });
-            MLO_.Clear();
+            Frame_Main_MainNv.Navigate(PageNv_Load);
         }
         private void _Closing(object sender, System.ComponentModel.CancelEventArgs e)
         {
             // 弹窗提示是否确定要退出
-            MessageBoxResult f1 = MessageBox.Show(App.IL_all_list[0], App.IL_all_list[0], MessageBoxButton.OKCancel, MessageBoxImage.Question);
+            MessageBoxResult f1 = MessageBox.Show("确定退出吗?", "?", MessageBoxButton.OKCancel, MessageBoxImage.Question);
             if (f1 == MessageBoxResult.OK)
             {
                 //文本文件中的第一行内容获取，并且被设置为app的屏幕值
                 //保存软件的屏幕位置和大小后退出
-                List<string> setting_homelist = File.ReadAllText(App.setting_path).Split("${@}").ToList();
-                string less = File.ReadAllText(App.setting_path);
-                string App_hwlt = Height + "," + Width + "," + Left + "," + Top;
-                setting_homelist[0] = App_hwlt; setting_homelist[8] = "8";
-                string Data = string.Join("${@}", setting_homelist);
-                StreamWriter sm1 = new StreamWriter(App.setting_path);
-                sm1.Write(Data);
+                string folderName = "Appdata";
+                string folderPath = System.IO.Path.Combine(AppDomain.CurrentDomain.BaseDirectory, folderName);
+                string filePath = System.IO.Path.Combine(folderPath, "data.txt");
+                string listText = File.ReadAllText(filePath);
+                List<string> setting_homelist = listText.Split("_").ToList();
+                string App_hwlt = "//屏幕_" + Height + "_" + Width + "_" + Left + "_" + Top;
+                StreamWriter sm1 = new StreamWriter(filePath);
+                sm1.Write(App_hwlt);
                 sm1.Close();
                 Process.GetCurrentProcess().Kill();
             }
